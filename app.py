b67bf168433637dfc44789406a97f9770340ccf6
Ubuntu <ubuntu@ip-172-31-88-68.ec2.internal>
2019-12-29 07:46:47 +0000

    Edited routing on app.py

--- app.py
+++ app.py
@@ -4,29 +4,32 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@cluster0-y00j5.mongodb.net/Goodreads?retryWrites=true&w=majority'
+app.config["MONGO_DBNAME"] = 'goodreads'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@cluster0-y00j5.mongodb.net/goodreads?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
 @app.route('/')
-@app.route('/get_tasks')
-def get_tasks():
-    return render_template("tasks.html", 
-                           tasks=mongo.db.tasks.find())
+@app.route('/get_books')
+def get_books():
+    return render_template("get_books.html", books=mongo.db.books.find())
+    
+
+# @app.route('/get_tasks')
+# def get_tasks():
+#     return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
+# @app.route('/add_task')
+# def add_task():
+#     return render_template('addtask.html',
+#                           categories=mongo.db.categories.find())
 
 
-@app.route('/add_task')
-def add_task():
-    return render_template('addtask.html',
-                           categories=mongo.db.categories.find())
-
-
-@app.route('/insert_task', methods=['POST'])
-def insert_task():
-    tasks = mongo.db.tasks
-    tasks.insert_one(request.form.to_dict())
-    return redirect(url_for('get_tasks'))
+# @app.route('/insert_task', methods=['POST'])
+# def insert_task():
+#     tasks = mongo.db.tasks
+#     tasks.insert_one(request.form.to_dict())
+#     return redirect(url_for('get_tasks'))
 
 
 if __name__ == '__main__':
