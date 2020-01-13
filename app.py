c02e3218c9fa737ae413fe1915bb16fe45ef44e2
Ubuntu <ubuntu@ip-172-31-88-68.ec2.internal>
2020-01-13 07:31:28 +0000

    added new requirements.txt file

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
-from flask_pymongo import PyMongo, DESCENDING
+from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 import re
 
