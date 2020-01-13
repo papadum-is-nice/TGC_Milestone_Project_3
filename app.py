13fba3941b572f9ce2691deea6f2a0e60bf53dd2
Ubuntu <ubuntu@ip-172-31-88-68.ec2.internal>
2020-01-13 04:11:25 +0000

    Amended footer styling

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def search_books():
     
 @app.route('/get_books')
 def get_books():
-    books=mongo.db.books.find().limit(4)
+    books=mongo.db.books.find().limit(8)
     return render_template('books_collection.html', title="Books Collection", books=books)
     
 @app.route('/add_book')
